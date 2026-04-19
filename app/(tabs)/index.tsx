d0f7963b5251c0fac3e1b72aa3e77aecab7de190
Val <you@example.com>
2026-04-19 20:49:15 +0800

    prefinal exam

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text1}> HI!, Val Anthony C. Cinco</Text>
+      <Text style={styles.text2}>2nd Year Bachelor of Science in Information Technology</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text1: {
+    color: 'white',
+    fontSize: 30,
+  },
+  text2: {
+    color: 'white',
+    fontSize: 30,
+  },
+});
